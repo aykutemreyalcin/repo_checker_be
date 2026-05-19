0a7c228b36879d8b720888d14cb66040300a1879
aykut <aykutemyeyalcinn@gmail.com>
2026-05-19 18:42:17 +0200

    first commit

---
+++ src/main/java/repoChecker/repo_checker_be/RepoCheckerBeApplication.java
@@ -0,0 +1,13 @@
+package repoChecker.repo_checker_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoCheckerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoCheckerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/repoChecker/repo_checker_be/RepoCheckerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package repoChecker.repo_checker_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoCheckerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
